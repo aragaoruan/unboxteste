c81e154aa0558c7e88a01d451c2bcd7bdb21fde7
Ruan Aragao <aragao.ruan@gmail.com>
2020-07-10 16:07:36 -0300

    feat: colocando a navegacao de Drawer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,90 +1,21 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import { View, Text } from 'react-native';
-import OpenDrawer from '~/components/OpenDrawer';
+import Movies from './movies.route';
 
-import Movies from '~/page/Movies';
-import Trending from '~/page/Trending';
-import SearchMovie from '~/page/SearchMovie';
-import DetailsMovies from '~/page/DetailsMovies';
+const Drawer = createDrawerNavigator();
 
-interface Props {
-  title?: string;
-}
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-const Tabs: React.FC = () => {
+const Navigator: React.FC = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="SearchMovie"
-        component={SearchMovie}
-        options={{
-          title: 'Pesquisar',
-          tabBarIcon: ({ color }) => (
-            <View>
-              <Icon name="search" size={20} color={color} />
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
+    <Drawer.Navigator initialRouteName="Movies" drawerType="slide">
+      <Drawer.Screen
         name="Movies"
+        options={{ title: 'Filmes' }}
         component={Movies}
-        options={{
-          title: 'Filmes',
-          tabBarIcon: ({ color }) => (
-            <View>
-              <Icon name="local-movies" size={20} color={color} />
-            </View>
-          ),
-        }}
       />
-      <Tab.Screen
-        name="Trending"
-        component={Trending}
-        options={{
-          title: 'Trending',
-          tabBarIcon: ({ color }) => (
-            <View>
-              <Icon name="trending-up" size={20} color={color} />
-            </View>
-          ),
-        }}
-      />
-    </Tab.Navigator>
+      {/* <Drawer.Screen name="MeusDados" component={MeusDados} /> */}
+    </Drawer.Navigator>
   );
 };
 
-const stack: React.FC = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#fff',
-        },
-        headerLeft: () => <OpenDrawer />,
-        headerTitle: 'Filmes',
-      }}
-    >
-      <Stack.Screen name="Tabs" component={Tabs} />
-      <Stack.Screen
-        name="DetailsMovie"
-        component={DetailsMovies}
-        options={({ route }) => {
-          const { title } = route.params as Props;
-          return {
-            headerTitle: title || '',
-          };
-        }}
-      />
-    </Stack.Navigator>
-  );
-};
-
-export default stack;
+export default Navigator;
